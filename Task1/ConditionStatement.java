0d4c3fa73b30f21be5473cf6e7eec8e458331101
Ajaykumar Wade <92783730+AjayKumar049@users.noreply.github.com>
2025-04-21 17:37:22 +0530

    Update ConditionStatement.java

--- Task1/ConditionStatement.java
+++ Task1/ConditionStatement.java
@@ -1,1 +1,11 @@
+public class ConditionStatement {
+    public static void main(String[] args) {
+
+        int age = 18;
+        if(age > 17)
+            System.out.println("You're eligible for vote");
+        else
+            System.out.println("Not eligible for vote");
+    }
+}
 
